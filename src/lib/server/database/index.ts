315005cf41aa6356990634d53f6b7377a90f835e
Jovany <43921445+jovanyvelez@users.noreply.github.com>
2024-11-20 15:12:12 -0500

    Update index.ts

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,18 +1,11 @@
-import { env } from '$env/dynamic/private';
-import { dev } from '$app/environment';
+import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-import  * as schema from './schema';
+import * as schema from './schema';
 
-if(!env.DATABASE_URL) throw new Error('DATABASE_URL no existe ')
-if(!env.DATABASE_URL) throw new Error('DATABASE_URL no existe ')
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL no existe ');
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL no existe ');
 
-console.log(env.DATABASE_URL)
-console.log(env.TURSO_AUTH_TOKEN)
+const client = createClient({ url: process.env.DATABASE_URL, authToken: process.env.TURSO_AUTH_TOKEN });
 
-if (!dev && !env.DATABASE_TOKEN) throw new Error('DATABASE_AUTH_TOKEN no fue definida en entorno');
-
-const client = createClient({ url: env.DATABASE_URL,
-                              authToken: env.TURSO_AUTH_TOKEN });
-
-export const db = drizzle(client, {schema, casing: 'snake_case' });
+export const db = drizzle(client, { schema, casing: 'snake_case' });
